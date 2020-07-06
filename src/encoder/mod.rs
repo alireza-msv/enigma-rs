1e351348da0d1ab73e11645eddef108894c39d62
Neo <alghoochi@gmail.com>
2020-07-06 20:20:53 +0430

    rotating rotors mechanism defined

--- src/encoder/mod.rs
+++ src/encoder/mod.rs
@@ -3,18 +3,30 @@ pub mod encoder {
 
     pub fn encode(rotors: &mut [String; 3], input: &String) -> String {
         let mut encoded: String = String::new();
+        let mut encoded_chars: usize = 0;
 
         for c in input.chars() {
             let mut ch: char = c;
+            encoded_chars += 1;
 
             for r in rotors.into_iter() {
                 ch = encode_char(&r, ch);
             }
-            
+
             ch = reflect(ch);
             
             for r in rotors.into_iter().rev() {
                 ch = encode_char_rev(&r, ch);
+            }
+
+            rotors[0] = rotate(&rotors[0]);
+            
+            if encoded_chars % ALPHABETS.len() == 0 {
+                rotors[1] = rotate(&rotors[1]);
+            }
+
+            if encoded_chars % (ALPHABETS.len() * ALPHABETS.len()) == 0 {
+                rotors[2] = rotate(&rotors[2]);
             }
 
             encoded.push(ch);
@@ -42,5 +54,13 @@ pub mod encoder {
         let reflected = ALPHABETS.chars().nth(index).unwrap();
     
         reflected
+    }
+
+    fn rotate(rotor: &String) -> String {
+        let mut rotated = String::new();
+        rotated.push_str(&rotor[1..]);
+        rotated.push(rotor.chars().nth(0).unwrap());
+
+        rotated
     }
 }
